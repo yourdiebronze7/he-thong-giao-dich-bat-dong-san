653b64b65bcb22d7a053bafa6bd96528cafea3c0
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-30 14:39:32 +0000

    Update secret value for better security practice

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   database: process.env.MONGODB_URI || 'mongodb://localhost:27017/real-estate',
-  secret: 'yourJWTSecret'
+  secret: process.env.JWT_SECRET || 'yourJWTSecret'
 };
